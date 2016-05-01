ffad2413344c21ca283fe8093389cd52f2dc7679
MTae <hmtri1011@gmail.com>
2016-05-02 00:36:28 +0700

    #

--- js/profile-controller.js
+++ js/profile-controller.js
@@ -5,20 +5,37 @@
         .module('app-profile')
         .controller('ProfileController',ProfileController);
 
-    ProfileController.$inject = ['$http'];
-    function ProfileController($http) {
+    ProfileController.$inject = ['$http','$scope'];   
+    function ProfileController($http,$scope) {
+        window.SC = $scope;
         var vm = this;
         
         vm.profile = {};
-        
+        vm.IsHide = true;
+        vm.Hide = Hide;
         activate();
-
+        
         ////////////////    
 
         function activate() {
             $http.get('data/profile.json').then(function(response){
                 vm.profile = response.data.info;
             })
+        }
+        
+        function Hide(){
+            vm.IsHide = !vm.IsHide;
+        }
+        
+        $scope.ChangeName = function(){
+            var firstname = $scope.firstname;
+            var lastname = $scope.lastname;
+            return (firstname + lastname);
+        }
+        
+        $scope.Submit = function(){
+            vm.profile.fullname = $scope.ChangeName();
+            vm.IsHide = !vm.IsHide;
         }
     }
 })();
